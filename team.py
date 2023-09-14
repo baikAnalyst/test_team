74bbfc4a995ac1ef55678fdc51863f7f1fcecf3a
yoonandmoon <dbstjs061@gmail.com>
2023-09-14 13:49:11 +0900

    오름차순

--- team.py
+++ team.py
@@ -11,11 +11,13 @@ def f_sum(data):
 
 
 #오름차순으로 정렬해주는 함수
-def f_sort(data):
-    #코드를 입력해주세요
-    return 1
+def f_sort(list):
+    list.sort()
+    return list
 
 #내림차순으로 정렬해주는 함수
 def f_desc(data):
     list = data.sort(reverse=True)
     return 1
+
+
