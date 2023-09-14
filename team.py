55df027afdf4a3d783cd71dd317801c649d66a42
bailAnalyst <baik.fintech.analyst@gmail.com>
2023-09-14 11:48:12 +0900

    '함수 4개로 변경'

--- team.py
+++ team.py
@@ -7,3 +7,13 @@ def f_avg(list):
 def f_sum(list):
     #코드를 입력해주세요
     return
+
+#오름차순으로 정렬해주는 함수
+def f_sort(list):
+    #코드를 입력해주세요
+    return
+
+#내림차순으로 정렬해주는 함수
+def f_desc(list):
+    #코드를 입력해주세요
+    return
