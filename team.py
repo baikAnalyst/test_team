f92c169aef53d43de98d8b77a0860368a648ffb1
bailAnalyst <baik.fintech.analyst@gmail.com>
2023-09-14 12:00:33 +0900

    data로 인자수정

--- team.py
+++ team.py
@@ -1,17 +1,17 @@
 # 평균을 구하는 함수
-def f_avg(list):
+def f_avg(data):
     # 코드를 입력해주세요
-    return
+    return 1
 
 #합계를 구하는 함수
-def f_sum(list):
+def f_sum(data):
     #코드를 입력해주세요
-    return
+    return 1
 
 #오름차순으로 정렬해주는 함수
-def f_sort(list):
+def f_sort(data):
     #코드를 입력해주세요
-    return
+    return 1
 
 #내림차순으로 정렬해주는 함수
 def f_desc(list):
