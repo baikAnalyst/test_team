4ad6d6cc689cae58dbb050be592c50ef67726a58
LeeJiwoo982 <easywoo980902@gmail.com>
2023-09-14 13:49:54 +0900

    주석에 이름 넣었음^^

--- team.py
+++ team.py
@@ -1,21 +1,20 @@
-# 평균을 구하는 함수
+# 평균을 구하는 함수.yumi
 def f_avg(data):
     list = sum(data)/len(data)
     return list
 
-#합계를 구하는 함수
+#합계를 구하는 함수.baik
 def f_sum(data):
   result = sum(data)
   return result
 
 
-
-#오름차순으로 정렬해주는 함수
+#오름차순으로 정렬해주는 함수.yoonsun
 def f_sort(data):
     #코드를 입력해주세요
     return 1
 
-#내림차순으로 정렬해주는 함수
+#내림차순으로 정렬해주는 함수.ijioo
 def f_desc(data):
     list = data.sort(reverse=True)
     return list
